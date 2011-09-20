953890a1454a685760d66490ef0a9ad71ebd0cc8
daniel <daniel@petraflux.com>
2011-09-20 23:14:32 +0200

    Rewrote code to work with tables instead of unordered lists

--- README.markdown
+++ README.markdown
@@ -1,44 +1,59 @@
 # Usage
 
 Basic options
-	$('#container').stickySectionHeaders();
+	$('table').stickyTableSectionHeaders();
 
-Check out the [demo here](http://www.polarblau.com/code/jquery/stickysectionheaders).
-	
 Settings and defaults
-	$('#container').stickySectionHeaders({
-		stickyClass      : 'sticky',
-		headlineSelector : 'strong'
+	$('table').stickyTableSectionHeaders({
+		rowSelector      : 'tr.sticky',
+		headlineSelector : 'th'
 	});
-	
-	
+
 Requires basic HTML structure
-	<div id="container">
-		<ul class="sections">
-			<li>
-				<strong>Section Headline 1</strong>
-				<ul>
-					<li id="id-1">Content line</li>
-					<li id="id-2">Content line</li>
-					<li id="id-3">Content line</li>
-					<li id="id-4">Content line</li>
-				</ul>
-			</li>
-			<li>
-				<strong>Section Headline 2</strong>
-				<ul>
-					<li id="id-5">Content line</li>
-					<li id="id-6">Content line</li>
-				</ul>
-			</li>
-			<li>
-				<strong>Section Headline 4</strong>
-				<ul>
-					<li id="id-14">Content line</li>
-					<li id="id-15">Content line</li>
-					<li id="id-16">Content line</li>
-					<li id="id-17">Content line</li>
-				</ul>
-			</li>
-		</ul>          
-	</div>
+    <table>
+    	<thead>
+    		<th colspan="3">Table</th>
+    	</thead>
+        <tbody>
+        	<tr class="sticky">
+        	    <th>Company</th>
+        	    <th>Name</th>
+        	    <th>Phone</th>
+        	</tr>
+        	<tr>
+        	    <td>in,</td>
+        	    <td>Naida Ashley</td>
+        	    <td>22 69 62 66</td>
+        	</tr>
+        	<tr>
+        	    <td>nisi a</td>
+        	    <td>Eaton Petty</td>
+        	    <td>22 88 94 57</td>
+        	</tr>
+        	<tr>
+        	    <td>scelerisque scelerisque</td>
+        	    <td>Harlan Mays</td>
+        	    <td>22 55 12 08</td>
+        	</tr>
+        	<tr class="sticky">
+        	    <th>Company</th>
+        	    <th>Name</th>
+        	    <th>Phone</th>
+        	</tr>
+        	<tr>
+        	    <td>in,</td>
+        	    <td>Naida Ashley</td>
+        	    <td>22 69 62 66</td>
+        	</tr>
+        	<tr>
+        	    <td>nisi a</td>
+        	    <td>Eaton Petty</td>
+        	    <td>22 88 94 57</td>
+        	</tr>
+        	<tr>
+        	    <td>scelerisque scelerisque</td>
+        	    <td>Harlan Mays</td>
+        	    <td>22 55 12 08</td>
+        	</tr>
+		</tbody>
+	</table>
---
+++ jquery.stickyTableSectionHeaders.js
@@ -0,0 +1,75 @@
+/*!
+ * Sticky Table Section Headers
+ *
+ * Copyright (c) 2011 Daniel Staver ( daniel@ixd.no / http://www.ixd.no )
+ * Loosely based on Sticky Section Headers Copyright (c) 2010 Florian Plank (http://www.polarblau.com/)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * USAGE:
+ *
+ * $('table').stickyTableSectionHeaders({
+ *     rowSelector      : 'tr.sticky',
+ *     headlineSelector : 'th'
+ * });
+ */
+
+(function($){
+    $.fn.stickyTableSectionHeaders = function(options) {
+        var settings = $.extend({
+            rowSelector: 'tr.sticky',
+            headlineSelector: 'th'
+        }, options);
+
+        return $(this).each( function() {
+            var table = $(this);
+
+            // Each TH needs hardcoded widths in px, otherwise the sticky header will shrink
+            // once the TR element gets position: fixed
+            table.find( settings.rowSelector + ' ' + settings.headlineSelector ).each(function(){
+                $(this).css({
+                    width: $(this).width() + 'px' // Hardcode calculated width
+                });
+            });
+
+            $(window).scroll( function(e) {
+                table.find( settings.rowSelector ).each(function(index) {
+                    var tr       = $(this),
+                        trTop    = tr.position().top - $(window).scrollTop(),
+                        trHeight = tr.outerHeight(),
+                        sticky   = $('table#sticky'),
+                        found    = sticky.length;
+
+                    // Create the sticky header element once, if it was not already found
+                    if( !found ) {
+                        console.log('CReate stiCky!');
+                        sticky = $('<table id="sticky" />').append($('<tbody />')).append(tr.clone());
+                        sticky.hide();
+                        sticky.css({
+                            position: 'fixed',
+                            top: 0,
+                            zIndex: 10000
+                        });
+                        $('body').append(sticky);
+                    }
+
+                    if( trTop < 0 ) {
+                        sticky.css({
+                            top: 0
+                        });
+                        sticky.show();
+                    }
+                    else if( trTop >= 0 && trTop <= trHeight ) {
+                        sticky.css({
+                            top: '-' + ( trHeight - trTop ) + 'px'
+                        });
+                        sticky.show();
+                    }
+                    else if( index == 0 ) {
+                        sticky.hide();
+                    }
+                });
+            });
+        });
+    };
+})(jQuery);
