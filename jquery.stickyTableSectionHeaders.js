5ae245318d8e905c0c589cb3c1b057a440a8c3a0
Daniel Staver <daniel@ixd.no>
2011-09-22 18:38:40 +0200

    Refactored some code. Made main header sticky

--- jquery.stickyTableSectionHeaders.js
+++ jquery.stickyTableSectionHeaders.js
@@ -9,93 +9,167 @@
  * USAGE:
  *
  * $('table').stickyTableSectionHeaders({
- *     rowSelector      : 'tr.sticky',
- *     headlineSelector : 'th'
+ *     section.rowSelector      : 'tr.sticky',
  * });
  */
-
 (function($){
     $.fn.stickyTableSectionHeaders = function(options) {
         var settings = $.extend({
-            rowSelector: 'tr.sticky',
-            headlineSelector: 'th'
+            head: {
+                sticky       : true, // Enable sticky header
+                rowSelector  : 'thead tr', // Selector to find the header row withing the table element
+                yOffset      : 0
+            },
+            section: {
+                sticky       : true,
+                rowSelector  : 'tr.sticky',
+                yOffset      : -4
+            }
         }, options);
 
         return $(this).each( function() {
-            var table = $(this);
+            var table                     = $(this),
+                tableBottom               = getBottomPosition(table), // Y position of table bottom relative to window
+                stickyHeaderSource        = null, // Holds the table row currently used as a source for sticky header.
+                stickySectionHeaderSource = null, // Holds the table row currently used as a source for sticky section header.
+                sectionYOffset            = 0;
 
-            var tableBottom = 0;
+            fixWidths( table ); // Hardcode PX widths on all header cells
 
-            // Holds the table row currently used as a source for sticky header.
-            var currentStickyTr = null;
+            if( settings.head.sticky ) {
+                var stickyHeader   = createSticky( table, '<thead><tr></tr></thead>', 10100, 'stickyHeader' );
+                stickyHeader.find('tr').replaceWith( table.find(settings.head.rowSelector).clone() );
+                sectionYOffset = stickyHeader.outerHeight();
+            }
 
-            // Each TH needs hardcoded widths in px, otherwise the sticky header will shrink
-            // once the TR element gets position: fixed
-            table.find( settings.rowSelector + ' ' + settings.headlineSelector ).each(function(){
+            if( settings.section.sticky ) {
+                var stickySectionHeader = createSticky( table, '<tbody><tr></tr></tbody>', 10000, 'stickySectionHeader' );
+            }
+
+            $(window).scroll( function(e) {
+                // Recalculate Y position of table bottom relative to window
+                tableBottom = getBottomPosition(table);
+
+                if( settings.head.sticky ) {
+                    table.find( settings.head.rowSelector ).each(function(index) {
+                        var tr       = $(this),
+                            trTop    = tr.offset().top - $(window).scrollTop(), // Y position of row relative to window
+                            trHeight = tr.outerHeight(); // Total height of row including borders and padding
+
+                        // Never show sticky if the header row is positioned inside the window or the whole table is positioned out of the window
+                        if(( trTop >= 0 && index === 0 ) || tableBottom < 0 ) {
+                            // console.log('hide');
+                            stickyHeader.hide();
+                        }
+
+                        // The current header row is somewhere out of the window and the table is at least partially visible, enough to show a full header row
+                        else if( trTop < 0 && tableBottom > trHeight ) {
+                            // console.log('stick');
+                            stickyHeader.css({
+                                top: 0
+                            });
+                            stickyHeader.show();
+                        }
+
+                        // Table bottom pushes sticky header out of window
+                        else if( tableBottom >= 0 && tableBottom <= trHeight ) {
+                            // console.log('push');
+                            stickyHeader.css({
+                                top: tableBottom - trHeight + 'px'
+                            });
+                            stickyHeader.show();
+                        }
+                    });
+                }
+
+                if( settings.section.sticky ) {
+                    table.find( settings.section.rowSelector ).each(function(index) {
+                        var tr       = $(this),
+                            trTop    = tr.offset().top - $(window).scrollTop(), // Y position of row relative to window
+                            trHeight = tr.outerHeight(); // Total height of row including borders and padding
+
+                        // Never show sticky if the first header row is positioned inside the window or the whole table is positioned out of the window
+                        if(( trTop >= sectionYOffset && index === 0 ) || tableBottom < sectionYOffset ) {
+                            // console.log('hide');
+                            stickySectionHeader.hide();
+                        }
+
+                        // The current header row is somewhere out of the window and the table is at least partially visible, enough to show a full header row
+                        else if( trTop < sectionYOffset && tableBottom > trHeight + sectionYOffset ) {
+                            // console.log('stick');
+                            stickySectionHeaderSource = tr;
+                            stickySectionHeader.css({
+                                top: settings.section.yOffset + sectionYOffset + 'px'
+                            });
+                            stickySectionHeader.show();
+                        }
+
+                        // A new header row is pushing the previous one out
+                        else if( index > 0 && trTop >= 0 && trTop <= trHeight + sectionYOffset ) {
+                            // console.log('push');
+                            stickySectionHeader.css({
+                                top: settings.section.yOffset + trTop - trHeight + 'px'
+                            });
+                            stickySectionHeader.show();
+                        }
+
+                        // Table bottom pushes sticky header out of window
+                        else if( tableBottom >= sectionYOffset && tableBottom <= trHeight + sectionYOffset ) {
+                            // console.log('bottom');
+                            stickySectionHeader.css({
+                                top: settings.section.yOffset + tableBottom - trHeight + 'px'
+                            });
+                            stickySectionHeader.show();
+                        }
+                    });
+
+                    // Replace contents of sticky header with contents from the row currently used as a source for the header
+                    if( stickySectionHeaderSource ) {
+                        stickySectionHeader.find('tr').replaceWith( stickySectionHeaderSource.clone() );
+                    }
+                }
+            });
+        });
+
+        // Get bottom position of element relative to window
+        function getBottomPosition( el ){
+            return el.offset().top + el.outerHeight() - $(window).scrollTop();
+        }
+
+        // Get unique ID for element
+        var uid = 0;
+        function getUID( prefix ){
+            uid++;
+            return prefix + uid;
+        }
+
+        // Create an empty sticky table based on a source table and append it to the DOM
+        function createSticky( srcTable, trHTML, zIndex, tableClass ) {
+            var sticky = $('<table id="' + getUID('sticky') + '" />').append($(trHTML)).hide().css({
+                position: 'fixed',
+                display: 'inline',
+                top: 0,
+                left: srcTable.offset().left + 'px',
+                zIndex: zIndex,
+                width: srcTable.width()
+            });
+            sticky.hide();
+            sticky.attr( 'class', srcTable.attr('class') + ' ' + tableClass );
+            $('body').append(sticky);
+            return sticky;
+        }
+
+        // Each TH needs hardcoded widths in px, otherwise the sticky header will shrink
+        // once the TR element gets position: fixed
+        function fixWidths( table ) {
+            table.find(
+                settings.section.rowSelector + ' th, ' +
+                settings.head.rowSelector    + ' th'
+            ).each(function(){
                 $(this).css({
                     width: $(this).width() + 'px' // Hardcode calculated width
                 });
             });
-
-            // Sticky table, a clone of the header row wrapped in a table
-            var sticky = $('<table id="sticky" />').append($('<tbody /><tr />')).hide().css({
-                position: 'fixed',
-                top: 0,
-                zIndex: 10000
-            });
-            $('body').append(sticky);
-
-            $(window).scroll( function(e) {
-                // Y position of table bottom relative to window
-                tableBottom = table.position().top + table.outerHeight() - $(window).scrollTop();
-
-                // Loop through each table header row
-                table.find( settings.rowSelector ).each(function(index) {
-                    var tr = $(this);
-
-                    // Y position of row relative to window
-                    var trTop = tr.position().top - $(window).scrollTop();
-
-                    // Total height of row including borders and padding
-                    var trHeight = tr.outerHeight();
-
-                    // Never show sticky if the first header row is positioned inside the window
-                    // or the whole table is positioned out of the window
-                    if(( trTop >= 0 && index === 0 ) || tableBottom < 0 ) {
-                        sticky.hide();
-                    }
-
-                    // The current header row is somewhere out of the window
-                    // and the table is at least partially visible, enough to show a full header row
-                    else if( trTop < 0 && tableBottom > trHeight ) {
-                        currentStickyTr = tr;
-                        sticky.css({
-                            top: 0
-                        });
-                        sticky.show();
-                    }
-
-                    // A new header row is pushing the previous one out
-                    else if( index > 0 && trTop >= 0 && trTop <= trHeight ) {
-                        sticky.css({
-                            top: '-' + ( trHeight - trTop ) + 'px'
-                        });
-                        sticky.show();
-                    }
-
-                    // Table bottom pushes sticky header out of window
-                    else if( tableBottom >= 0 && tableBottom <= trHeight ) {
-                        sticky.css({
-                            top: tableBottom - trHeight + 'px'
-                        });
-                        sticky.show();
-                    }
-                });
-
-                if( currentStickyTr ) {
-                    sticky.find('tr').replaceWith( currentStickyTr.clone() );
-                }
-            });
-        });
+        }
     };
 })(jQuery);
